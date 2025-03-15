68812b1e4ac91ec03f017efeb3d1011039021160
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2025-03-15 14:01:39 +0100

    Add EntityManager to manage asb queues/topics/subscriptions

--- src/Pigeon.AzureServiceBus/EntityManager.cs
+++ src/Pigeon.AzureServiceBus/EntityManager.cs
@@ -1,6 +1,80 @@
-﻿namespace Pigeon.AzureServiceBus;
+﻿using System.Collections.Concurrent;
 
-internal sealed class EntityManager
+namespace Pigeon.AzureServiceBus;
+
+// TODO Check how we need to configure the service-bus entities
+internal sealed class EntityManager(ServiceBusAdministrationClient administrationClient)
 {
+    private readonly ConcurrentDictionary<string, object> _entitySyncRoots = new();
 
+    public async Task CreateQueueIfNotExists(string queueName, CancellationToken cancellationToken = default)
+    {
+        if (!await administrationClient.QueueExistsAsync(queueName, cancellationToken))
+            await CreateQueue(queueName, cancellationToken);
+    }
+
+    public async Task CreateQueue(string queueName, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var createResponse = await administrationClient.CreateQueueAsync(queueName, cancellationToken);
+            if (createResponse.HasValue && createResponse.Value.Status == EntityStatus.Active)
+                return;
+        }
+        catch (ServiceBusException ex) when (ex.Reason == ServiceBusFailureReason.MessagingEntityAlreadyExists)
+        {
+            return;
+        }
+
+        throw new InvalidOperationException($"Creation of queue '{queueName}' failed");
+    }
+
+    public async Task CreateTopicIfNotExists(string topicName, CancellationToken cancellationToken = default)
+    {
+        if (!await administrationClient.TopicExistsAsync(topicName, cancellationToken))
+            await CreateTopic(topicName, cancellationToken);
+    }
+
+    public async Task CreateTopic(string topicName, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var createResponse = await administrationClient.CreateTopicAsync(topicName, cancellationToken);
+            if (createResponse.HasValue && createResponse.Value.Status == EntityStatus.Active)
+                return;
+        }
+        catch (ServiceBusException ex) when (ex.Reason == ServiceBusFailureReason.MessagingEntityAlreadyExists)
+        {
+            return;
+        }
+
+        throw new InvalidOperationException($"Creation of topic '{topicName}' failed");
+    }
+
+    public async Task CreateSubscriptionIfNotExists(string subscriptionName, string topicName, string queueName, CancellationToken cancellationToken = default)
+    {
+        if (!await administrationClient.SubscriptionExistsAsync(topicName, subscriptionName, cancellationToken))
+            await CreateSubscription(subscriptionName, topicName, queueName, cancellationToken);
+    }
+
+    public async Task CreateSubscription(string subscriptionName, string topicName, string queueName, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var createOptions = new CreateSubscriptionOptions(topicName, subscriptionName)
+            {
+                ForwardTo = queueName,
+            };
+
+            var createResponse = await administrationClient.CreateSubscriptionAsync(createOptions, cancellationToken);
+            if (createResponse.HasValue && createResponse.Value.Status == EntityStatus.Active)
+                return;
+        }
+        catch (ServiceBusException ex) when (ex.Reason == ServiceBusFailureReason.MessagingEntityAlreadyExists)
+        {
+            return;
+        }
+
+        throw new InvalidOperationException($"Creation of subscription '{subscriptionName}' failed");
+    }
 }
--- src/Pigeon.AzureServiceBus/Transport.cs
+++ src/Pigeon.AzureServiceBus/Transport.cs
@@ -3,11 +3,10 @@ using Pigeon.AzureServiceBus.Factories;
 
 namespace Pigeon.AzureServiceBus;
 
-// TODO Check how we need to configure the service-bus entities
 internal sealed class Transport(
     SenderFactory senderFactory,
     ProcessorFactory processorFactory,
-    ServiceBusAdministrationClient administrationClient,
+    EntityManager entityManager,
     ISubscriptionNamingConvention subscriptionNamingConvention) : ITransport
 {
     public async ValueTask Send(SerializedEnvelope envelope, CancellationToken cancellationToken = default)
@@ -25,13 +24,7 @@ internal sealed class Transport(
         }
         catch (ServiceBusException ex) when(ex.Reason == ServiceBusFailureReason.MessagingEntityNotFound)
         {
-            var response = await administrationClient.CreateTopicAsync(envelope.TopicName, cancellationToken);
-            if (!response.HasValue || response.Value.Status != EntityStatus.Active)
-            {
-                // TODO Throw explicit exception or log error
-                throw;
-            }
-
+            await entityManager.CreateTopicIfNotExists(envelope.TopicName, cancellationToken);
             await sender.SendMessageAsync(message, cancellationToken);
         }
     }
@@ -39,11 +32,11 @@ internal sealed class Transport(
     public async ValueTask StartListener(string queueName, string[] topicNames, CancellationToken cancellationToken = default)
     {
         var subscriptionName = subscriptionNamingConvention.Format(queueName);
-        await CreateQueueIfNotExists(queueName, cancellationToken);
+        await entityManager.CreateQueueIfNotExists(queueName, cancellationToken);
         foreach (var topicName in topicNames)
         {
-            await CreateTopicIfNotExists(topicName, cancellationToken);
-            await CreateSubscriptionIfNotExists(subscriptionName, topicName, queueName, cancellationToken);
+            await entityManager.CreateTopicIfNotExists(topicName, cancellationToken);
+            await entityManager.CreateSubscriptionIfNotExists(subscriptionName, topicName, queueName, cancellationToken);
         }
 
         var processor = processorFactory.GetOrCreateProcessor(queueName);
@@ -52,46 +45,5 @@ internal sealed class Transport(
         processor.ProcessErrorAsync += e => Task.CompletedTask;
 
         await processor.StartProcessingAsync(cancellationToken);
-    }
-
-    private async ValueTask CreateQueueIfNotExists(string queueName, CancellationToken cancellationToken)
-    {
-        if (await administrationClient.QueueExistsAsync(queueName, cancellationToken))
-            return;
-
-        var createResponse = await administrationClient.CreateQueueAsync(queueName, cancellationToken);
-        if (createResponse.HasValue && createResponse.Value.Status == EntityStatus.Active)
-            return;
-
-        throw new InvalidOperationException($"Creation of queue '{queueName}' failed");
-    }
-
-    private async ValueTask CreateTopicIfNotExists(string topicName, CancellationToken cancellationToken)
-    {
-        if (await administrationClient.TopicExistsAsync(topicName, cancellationToken))
-            return;
-
-        var createResponse = await administrationClient.CreateTopicAsync(topicName, cancellationToken);
-        if (createResponse.HasValue && createResponse.Value.Status == EntityStatus.Active)
-            return;
-
-        throw new InvalidOperationException($"Creation of topic '{topicName}' failed");
-    }
-
-    private async ValueTask CreateSubscriptionIfNotExists(string subscriptionName, string topicName, string queueName, CancellationToken cancellationToken)
-    {
-        if (await administrationClient.SubscriptionExistsAsync(topicName, subscriptionName, cancellationToken))
-            return;
-
-        var options = new CreateSubscriptionOptions(topicName, subscriptionName)
-        {
-            ForwardTo = queueName,
-        };
-
-        var createResponse = await administrationClient.CreateSubscriptionAsync(options, cancellationToken);
-        if (createResponse.HasValue && createResponse.Value.Status == EntityStatus.Active)
-            return;
-
-        throw new InvalidOperationException($"Creation of subscription '{queueName}' on topic '{topicName}' failed");
     }
 }
