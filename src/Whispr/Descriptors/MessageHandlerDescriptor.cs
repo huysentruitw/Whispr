a57b4d32d769cc62064714b0a300f1516b6c166c
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2025-12-03 21:01:52 +0100

    Make MessageHandlerDescriptor public to give insights about handler discovery

--- src/Whispr/Descriptors/MessageHandlerDescriptor.cs
+++ src/Whispr/Descriptors/MessageHandlerDescriptor.cs
@@ -1,8 +1,21 @@
 ﻿namespace Whispr.Descriptors;
 
-internal sealed record MessageHandlerDescriptor
+/// <summary>
+/// Describes a message handler, including all message-types it handles,
+/// detected by <see cref="WhisprBuilderExtensions.AddMessageHandlersFromAssembly"/>.
+///
+/// Retrieve <see cref="IEnumerable{MessageHandlerDescriptor}"/> from the DI container
+/// to get all detected message handlers. 
+/// </summary>
+public sealed record MessageHandlerDescriptor
 {
+    /// <summary>
+    /// The type of the handler.
+    /// </summary>
     public required Type HandlerType { get; init; }
 
+    /// <summary>
+    /// The message types supported by the handler.
+    /// </summary>
     public required Type[] MessageTypes { get; init; }
 }
