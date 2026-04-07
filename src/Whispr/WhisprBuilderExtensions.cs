e282b8669d6828da333d3a5549008961910c258c
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2026-04-07 22:33:09 +0200

    Add factory overloads for AddXxxFilter methods

--- src/Whispr/WhisprBuilderExtensions.cs
+++ src/Whispr/WhisprBuilderExtensions.cs
@@ -108,6 +108,24 @@ public static class WhisprBuilderExtensions
     }
 
     /// <summary>
+    /// Adds a publish filter using a factory method.
+    /// </summary>
+    /// <param name="builder">The <see cref="WhisprBuilder"/>.</param>
+    /// <param name="factory">A factory function that creates the filter instance. The function receives the service provider and bus name.</param>
+    /// <typeparam name="TFilter">The type of the publish filter.</typeparam>
+    /// <returns>The <see cref="WhisprBuilder"/>.</returns>
+    public static WhisprBuilder AddPublishFilter<TFilter>(
+        this WhisprBuilder builder,
+        Func<IServiceProvider, string, TFilter> factory)
+        where TFilter : class, IPublishFilter
+    {
+        builder.Services.AddKeyedScoped<IPublishFilter>(
+            builder.BusName,
+            (sp, key) => factory(sp, (string)key!));
+        return builder;
+    }
+
+    /// <summary>
     /// Adds a send filter.
     /// </summary>
     /// <param name="builder">The <see cref="WhisprBuilder"/>.</param>
@@ -121,6 +139,24 @@ public static class WhisprBuilderExtensions
     }
 
     /// <summary>
+    /// Adds a send filter using a factory method.
+    /// </summary>
+    /// <param name="builder">The <see cref="WhisprBuilder"/>.</param>
+    /// <param name="factory">A factory function that creates the filter instance. The function receives the service provider and bus name.</param>
+    /// <typeparam name="TFilter">The type of the send filter.</typeparam>
+    /// <returns>The <see cref="WhisprBuilder"/>.</returns>
+    public static WhisprBuilder AddSendFilter<TFilter>(
+        this WhisprBuilder builder,
+        Func<IServiceProvider, string, TFilter> factory)
+        where TFilter : class, ISendFilter
+    {
+        builder.Services.AddKeyedScoped<ISendFilter>(
+            builder.BusName,
+            (sp, key) => factory(sp, (string)key!));
+        return builder;
+    }
+
+    /// <summary>
     /// Adds a consume filter.
     /// </summary>
     /// <param name="builder">The <see cref="WhisprBuilder"/>.</param>
@@ -130,6 +166,24 @@ public static class WhisprBuilderExtensions
         where TFilter : class, IConsumeFilter
     {
         builder.Services.AddKeyedScoped<IConsumeFilter, TFilter>(builder.BusName);
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds a consume filter using a factory method.
+    /// </summary>
+    /// <param name="builder">The <see cref="WhisprBuilder"/>.</param>
+    /// <param name="factory">A factory function that creates the filter instance. The function receives the service provider and bus name.</param>
+    /// <typeparam name="TFilter">The type of the consume filter.</typeparam>
+    /// <returns>The <see cref="WhisprBuilder"/>.</returns>
+    public static WhisprBuilder AddConsumeFilter<TFilter>(
+        this WhisprBuilder builder,
+        Func<IServiceProvider, string, TFilter> factory)
+        where TFilter : class, IConsumeFilter
+    {
+        builder.Services.AddKeyedScoped<IConsumeFilter>(
+            builder.BusName,
+            (sp, key) => factory(sp, (string)key!));
         return builder;
     }
 
